bade5e75a46e32c5b9d613984285374f657f9b87
nyikhil <74239811+nyikhil@users.noreply.github.com>
2021-04-22 17:08:35 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,35 +1,35 @@
 class script(object):
 
-    START_TEXT = """<b>Hai ,
+    START_TEXT = """<b>Hello Friend ,
     
 I'm A simple Zee5 link downloader bot With Permanent Thumbnail Support💯.
 
 Please send me any Zee5 link, I can upload it to telegram as File/Video.
 
-Currently I'm in beta mode 🥺, If found any bugs, report @TroJanzSupport !
+Currently I'm in beta mode 🥺, If found any bugs, report @CCO_Chat_Bot !
 
 Click <i>/help</i> for more details....</b>"""
 
 
-    HELP_USER = """<b>Hai, Follow these steps..</b>
+    HELP_USER = """<b>Hello, Follow these steps..</b>
  
 1. Send Custom Thumbnail (It will be saved permenantly!)
 
-2. Send your zee5 url and select desired option.
+2. Send your Zee5 URL and select desired option.
 
 
 NOTE: Download may take some time! So please wait for it to complete!"""
 
 
-    ABOUT_TEXT = """⭕️<b>My Name : TroJanZ Zee5 DL</b>
+    ABOUT_TEXT = """⭕️<b>My Name : AceReaper Zee5 Downloader</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @AceReaper
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
-⭕️<b>Source Code :</b> 👉 <a href='https://github.com/TroJanzHEX/Zee5-Downloader'>Click Here</a>"""
+⭕️<b>Source Code :</b> 👉 <a href='https://github.com/nyikhil/Zee5-Downloader'>Click Here</a>"""
 
 
 
@@ -41,7 +41,7 @@ NOTE: Download may take some time! So please wait for it to complete!"""
 <i>NOTE : Taking high resolutions may result in files above 2GB and hence cannot Upload to TG. So better select a medium resolution.</i> 😇
 """    
     
-    UPGRADE_TEXT = "PING at @contactHEXbot"
+    UPGRADE_TEXT = "PING at @CCO_Chat_Bot"
     
     DOWNLOAD_START = "Trying to download to my server. This may take a while 😴"
     
@@ -59,7 +59,7 @@ NOTE: Download may take some time! So please wait for it to complete!"""
     
     NO_THUMB = "SED😕 No saved thumbnails Found!!"
     
-    CUSTOM_CAPTION_UL_FILE = "<b>{newname}\n\n©️ @TroJanzHEX</b>"
+    CUSTOM_CAPTION_UL_FILE = "<b>{newname}\n\n©️ @Cinema_Crave_Official</b>"
     
     TIMEOUT = "<b><i>Sorry for the delay. It'll help reduce the flood wait</i> 😇\n\nWait for {} sec and try again.</b>"
     
